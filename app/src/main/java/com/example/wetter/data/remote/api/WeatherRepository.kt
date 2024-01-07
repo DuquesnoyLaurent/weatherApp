6e45447fb780bbd899fed77e49c4e4be27341515
DuquesnoyLaurent <laurent.duquesnoy@student.hogent.be>
2024-01-07 15:57:16 +0100

    added api url parameters

--- app/src/main/java/com/example/wetter/data/remote/api/WeatherRepository.kt
+++ app/src/main/java/com/example/wetter/data/remote/api/WeatherRepository.kt
@@ -1,11 +1,16 @@
 package com.example.wetter.data.remote.api
 
-import retrofit2.http.GET
-import com.example.wetter.model.Location
 import com.example.wetter.data.remote.model.WeatherApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface WeatherRepository {
-    @GET("/weather/")
-    suspend fun getWeatherForLocation(location : Location) : WeatherApiResponse
+    //https://api.openweathermap.org/data/2.5/weather\?lat\=51.053822\&lon\=3.722270\&appid\=a0085eb0031511687f5f61d02f5770a0\&units\=metric
+    @GET("/weather/?lat={latitude}&lon={longitude}&appid={token}&units=metric")
+    suspend fun getWeatherForLocation(
+        @Path("latitude") latitude: String,
+        @Path("longitude") longitude: String,
+        @Path("token") token: String
+    ): WeatherApiResponse
 }
